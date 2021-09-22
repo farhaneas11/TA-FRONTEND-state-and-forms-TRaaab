0871316ea69ab2cd612ea6d29b93c3498969be14
farhaneas11 <farhan.salam11@gmail.com>
2021-09-22 17:04:02 +0530

    state-forms-react-12

---
+++ block-BRaabk/code/food-menu/src/components/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from "./header";
+import FoodTag from "./foodTag";
+//import FoodList from "./foodList";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <FoodTag/>            
+        </>
+        
+    );
+}
+export default App;
