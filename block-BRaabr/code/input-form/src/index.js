52a44115e6d44e37a1b01adf5aee0fda3ea53f03
farhaneas11 <farhan.salam11@gmail.com>
2021-09-25 10:52:14 +0530

    state-forms-react-32

---
+++ block-BRaabr/code/input-form/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Multiple from "./component/multiple";
+import Validation from "./component/validation";
+import './index.css';
+
+ReactDOM.render(
+    <Multiple />
+    <Validation />
+    ,
+        
+)
